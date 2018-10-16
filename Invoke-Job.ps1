f3a3c0759d745cc2725cfb3dd7fb0c28b553492d
spctravis <spctravis@gmail.com>
2018-10-15 21:13:36 -0600

    Update Invoke-Job.ps1
    Changed Return to xml

--- Invoke-Job.ps1
+++ Invoke-Job.ps1
@@ -1,9 +1,10 @@
-﻿
 #Sets date format
 $date = Get-Date -Format "MMddyyyyTHHmmss"
 
 #Set Job Name here, uses date by default
 $JobName = $date
+
+$session = Get-PSSession
 
 #Pushes you command into remote systems. Only change the scriptblock ${function:Please-CHANGEME}
 $AsJob = Invoke-Command -Session $session -ScriptBlock ${function:get-hostbaseline} -JobName $JobName -AsJob # -ErrorAction SilentlyContinue
@@ -11,3 +12,5 @@ $AsJob = Invoke-Command -Session $session -ScriptBlock ${function:get-hostbaseli
 $AsJob | wait-job 
 
 $returndata = $AsJob | Receive-Job 
+
+$returndata | Export-Clixml -Path C:\Users\Twitchy\Documents\WorkingDir\$jobname.xml
